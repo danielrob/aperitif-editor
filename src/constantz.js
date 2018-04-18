dcba4d6f8d39d999f167c1ffc98b1aa36d1439cb
Daniel Robinson <djr.self@gmail.com>
2018-04-19 01:45:11 +1200

    Move constants.js => constantz.js

---
+++ src/constantz.js
@@ -0,0 +1,12 @@
+export const required = 'required'
+
+export const fileTypes = {
+  JS: 'js',
+  DIR: 'dir',
+}
+
+export const templateTypes = {
+  STATELESS_FUNCTION_COMPONENT: 'stateless_function_component',
+  STYLED_COMPONENT: 'styled_component',
+  STANDARD: 'standard',
+}
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,3 @@
 import classNames from 'classnames'
 
 export const addClassNames = (...args) => ({ className: classNames(...args) })
-
-// get an arbritrary app-unique value for a type dictionary
-export const whatev = (incr => () => incr++)(1) // eslint-disable-line no-param-reassign
