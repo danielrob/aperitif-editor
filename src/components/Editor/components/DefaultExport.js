d93ce4507732119767e85a7a1df0be55e3d24b2b
Daniel Robinson <djr.self@gmail.com>
2018-05-02 17:30:58 +1200

    Keyword colors for 'export default'

--- src/components/Editor/components/DefaultExport.js
+++ src/components/Editor/components/DefaultExport.js
@@ -1,8 +1,13 @@
 import React from 'react'
-import { Line } from 'components'
+import { Line, Keyword } from 'components'
 
-const DefaultExport = ({ name }) => (
-  name ? <Line statement>export default {name}</Line> : null
-)
+const DefaultExport = ({ name }) =>
+  name ? (
+    <Line statement>
+      <Keyword>export default</Keyword>
+      {' '}
+      {name}
+    </Line>
+  ) : null
 
 export default DefaultExport
