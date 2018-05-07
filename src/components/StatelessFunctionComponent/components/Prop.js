ab99cedbe33c1793479150c3b10149393463fc16
Daniel Robinson <djr.self@gmail.com>
2018-05-08 11:53:49 +1200

    Don't show comma in prop preview

--- src/components/StatelessFunctionComponent/components/Prop.js
+++ src/components/StatelessFunctionComponent/components/Prop.js
@@ -4,17 +4,20 @@ import styled from 'styled-as-components'
 import theme from 'theme-proxy'
 import { spaces } from 'utils'
 
-const Prop = ({ name, isLast, connectDragSource }) =>
-  connectDragSource(
-    <span
-      style={{
-        userSelect: 'text',
-      }}
-    >
-      {name}
-      {!isLast && `,${spaces(1)}`}
-    </span>
-  )
+const Prop = ({ name, isLast, connectDragSource }) => (
+  <React.Fragment>
+    {connectDragSource(
+      <span
+        style={{
+          userSelect: 'text',
+        }}
+      >
+        {name}
+      </span>
+    )}
+    {!isLast && `,${spaces(1)}`}
+  </React.Fragment>
+)
 
 export default styled(Prop).as.div`
   display: inline-block;
