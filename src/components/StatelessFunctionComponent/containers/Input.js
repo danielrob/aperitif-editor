f18076b7d539efff123c472d82cd29d5d86364aa
Daniel Robinson <djr.self@gmail.com>
2018-05-08 22:59:17 +1200

    Mask ComponentNames

--- src/components/StatelessFunctionComponent/containers/Input.js
+++ src/components/StatelessFunctionComponent/containers/Input.js
@@ -5,22 +5,31 @@ import { createStructuredSelector } from 'reselect'
 import { makeSelectName } from 'selectors'
 import AutosizeInput from 'react-input-autosize'
 
-
 class Input extends React.Component {
-  onChange = (e) => {
+  onChange = e => {
     const { changeName, nameId } = this.props
-    changeName({
-      nameId,
-      value: e.target.value,
-    })
+    const pos = e.target.selectionStart - 1
+    const nextName = e.target.value
+
+    if (nextName && !(/^(?![0-9])[a-zA-Z0-9]+$/.exec(nextName))) {
+      this.inputRef.setSelectionRange(pos, 0)
+      setTimeout(() => this.inputRef.setSelectionRange(pos, 0))
+      return
+    }
+
+    changeName({ nameId, value: nextName })
   }
+
   render() {
+    const { name } = this.props
     const input = {
-      value: this.props.name,
+      value: name,
       onChange: this.onChange,
     }
 
-    return (<AutosizeInput type="text" {...input} />)
+    return (
+      <AutosizeInput inputRef={ref => { this.inputRef = ref }} type="text" {...input} />
+    )
   }
 }
 
